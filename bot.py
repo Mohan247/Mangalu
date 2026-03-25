91e5fcd8a5db49a36c3dfae3eda7d5325f0a7080
Mohan247 <discipline100times@gmail.com>
2026-03-25 14:57:19 +0530

    Update environment variable names for bot configuration

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
 import os
 
-api_id = int(os.getenv("23699522"))
-api_hash = os.getenv("399ecb6b9f12118466dba00efd6e5fac")
-bot_token = os.getenv("8720044154:AAHFvpAjPFNPE23DlEHm2Xci8n9vfQf_OQ0")
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+bot_token = os.getenv("BOT_TOKEN")
 
 app = Client("bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
